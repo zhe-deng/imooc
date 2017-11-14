cd0ead22b3396808d85132e84f3abe3df629af26
fdeng <frank.deng@morningstar.com>
2017-11-14 23:31:06 +0800

    1. Add grunt task. 2. Add signup and login entrance.

---
+++ node-mongodb/schemas/user.js
@@ -0,0 +1,67 @@
+var mongoose = require('mongoose');
+var bcrypt = require('bcrypt');
+var SALT_WORK_FACTOR = 10;
+
+var MovieSchema = new mongoose.Schema({
+    name: {
+        unique: true,
+        type: String
+    },
+    password: String,
+    meta: {
+        createAt: {
+            type: Date,
+            default: Date.now()
+        },
+        updateAt: {
+            type: Date,
+            default: Date.now()
+        }
+    }
+});
+
+// pre('save') 意思是说每次在存储数据之前都会来调用一次这个方法
+MovieSchema.pre('save', function(next) {
+    var user = this;
+
+    if (this.isNew) {
+        this.meta.createAt = this.meta.updateAt = Date.now();
+    } else {
+        this.meta.updateAt = Date.now();
+    }
+
+    bcrypt.genSalt(SALT_WORK_FACTOR, function(err, salt) {
+        if (err) {
+            return next(err);
+        }
+
+        bcrypt.hash(user.password, salt, function(err, hash) {
+            if (err) {
+                return next(err);
+            }
+
+            user.password = hash;
+
+            next();
+        });
+    });
+
+    next();
+});
+
+// 添加静态方法：这些静态方法不会直接跟数据库交互，只有通过model并且实例化之后才会有这个方法
+MovieSchema.statics = {
+    fetch: function(cb) {
+        return this
+            .find({})
+            .sort('meta.updateAt')
+            .exec(cb);
+    },
+    findById: function(id, cb) {
+        return this
+            .findOne({ _id: id })
+            .exec(cb);
+    }
+};
+
+module.exports = MovieSchema;
